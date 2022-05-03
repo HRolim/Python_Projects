e9d811872a072d800dcc78c736207ab234ed37f1
HRolm <rolimheitor@gmail.com>
2022-05-03 15:10:48 -0300

    without commit

--- aula 5/exercicio3.py
+++ aula 5/exercicio3.py
@@ -1,1 +1,20 @@
-print("Heitor"[:4])
+print("=+Calcule o Lucro+=")
+
+nome = input("Insira o nome do produto: ")
+valor = float(input("Insira o valor do produto: "))
+
+if valor < 10.00:
+    valor = valor + (valor * 0.7)
+    print(nome,"terá o valor da venda de",valor)
+
+elif 10.00 <= valor < 30.00:
+    valor = valor + (valor *0.5)
+    print(nome,"terá o valor da venda de",valor)
+
+elif 30.00 <= valor < 50.00:
+    valor = valor + (valor *0.4)
+    print(nome,"terá o valor da venda de",valor)
+
+elif valor >= 50.00:
+    valor = valor + (valor *0.3)
+    print(nome,"terá o valor da venda de",valor)
